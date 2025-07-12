6f8391a7865f2487f5bda6e87e5b8071230b5330
nurioget <nurioget9@gmail.com>
2025-07-13 00:18:17 +0300

    CafeInfo Services

--- Core/CaffeAPI.Aplication/Services/Concrete/CaffeInfoServices.cs
+++ Core/CaffeAPI.Aplication/Services/Concrete/CaffeInfoServices.cs
@@ -4,6 +4,7 @@ using CaffeAPI.Aplication.Dtos.ResponseDtos;
 using CaffeAPI.Aplication.Interfaces;
 using CaffeAPI.Aplication.Services.Abstract;
 using CaffeAPI.Domain.Entities;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,21 +17,53 @@ namespace CaffeAPI.Aplication.Services.Concrete
     {
         private readonly IGenericRepository<CaffeInfo> _caffeInfoRepository;
         private readonly IMapper _mapper;
+        private readonly IValidator<CreateCaffeInfoDto> _createCaffeInfoValidator;
+        private readonly IValidator<UpdateCaffeInfoDto> _updateCaffeInfoValidator;
 
-        public CaffeInfoServices(IGenericRepository<CaffeInfo> caffeInfoRepository, IMapper mapper)
+        public CaffeInfoServices(IGenericRepository<CaffeInfo> caffeInfoRepository, IMapper mapper, IValidator<CreateCaffeInfoDto> createCaffeInfoValidator, IValidator<UpdateCaffeInfoDto> updateCaffeInfoValidator)
         {
             _caffeInfoRepository = caffeInfoRepository;
             _mapper = mapper;
+            _createCaffeInfoValidator = createCaffeInfoValidator;
+            _updateCaffeInfoValidator = updateCaffeInfoValidator;
         }
 
-        public Task<ResponseDto<object>> AddCaffeInfo(CreateCaffeInfoDto dto)
+        public async Task<ResponseDto<object>> AddCaffeInfo(CreateCaffeInfoDto dto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var validate = await _createCaffeInfoValidator.ValidateAsync(dto);
+                if (!validate.IsValid)
+                {
+                    return new ResponseDto<object> { Success = false, Data = null, Message = validate.Errors.Select(x => x.ErrorMessage).FirstOrDefault(), ErrorCode = ErrorCodes.ValidationError };
+                }
+                var caffeInfo = _mapper.Map<CaffeInfo>(dto);
+                await _caffeInfoRepository.AddAsync(caffeInfo);
+                return new ResponseDto<object> { Success = true, Data = null, Message = "Kafe bilgisi başarıyla eklendi" };
+
+            }
+            catch (Exception ex)
+            {
+                return new ResponseDto<object> { Success = false, Data = null, Message = "Bir Hata Oluştu", ErrorCode = ErrorCodes.Exception };
+            }
         }
 
-        public Task<ResponseDto<object>> DeleteCaffeInfo(int id)
+        public async Task<ResponseDto<object>> DeleteCaffeInfo(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var caffeInfo = await _caffeInfoRepository.GetByIdAsync(id);
+                if (caffeInfo == null)
+                {
+                    return new ResponseDto<object> { Success = false, Data = null, Message = "Kafe bilgisi bulunamadı", ErrorCode = ErrorCodes.NotFound };
+                }
+                await _caffeInfoRepository.DeleteAsync(caffeInfo);
+                return new ResponseDto<object> { Success = true, Data = null, Message = "Kafe bilgisi başarıyla silindi" };
+            }
+            catch (Exception)
+            {
+                return new ResponseDto<object> { Success = false, Data = null, Message = "Bir Hata Oluştu", ErrorCode = ErrorCodes.Exception };
+            }
         }
 
         public async Task<ResponseDto<List<ResultCaffeInfoDto>>> GetAllCafeInfos()
@@ -52,14 +85,48 @@ namespace CaffeAPI.Aplication.Services.Concrete
             }
         }
 
-        public Task<ResponseDto<DetailCaffeInfoDto>> GetByIdCafeInfo(int id)
+        public async Task<ResponseDto<DetailCaffeInfoDto>> GetByIdCafeInfo(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var caffeInfo = await _caffeInfoRepository.GetByIdAsync(id);
+                if (caffeInfo == null)
+                {
+                    return new ResponseDto<DetailCaffeInfoDto> { Success = false, Data = null, Message = "Kafe bilgisi bulunamadı", ErrorCode = ErrorCodes.NotFound };
+                }
+                var result = _mapper.Map<DetailCaffeInfoDto>(caffeInfo);
+                return new ResponseDto<DetailCaffeInfoDto> { Success = true, Data = result };
+            }
+            catch (Exception ex)
+            {
+
+                return new ResponseDto<DetailCaffeInfoDto> { Success = false, Data = null, Message = "Bir Hata Oluştu", ErrorCode = ErrorCodes.Exception };
+            }
         }
 
-        public Task<ResponseDto<object>> UpdateCaffeInfo(UpdateCaffeInfoDto dto)
+        public async Task<ResponseDto<object>> UpdateCaffeInfo(UpdateCaffeInfoDto dto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var validate = await _updateCaffeInfoValidator.ValidateAsync(dto);
+                if (!validate.IsValid)
+                {
+                    return new ResponseDto<object> { Success = false, Data = null, Message = validate.Errors.Select(x => x.ErrorMessage).FirstOrDefault(), ErrorCode = ErrorCodes.ValidationError };
+                }
+                var caffeInfo = await _caffeInfoRepository.GetByIdAsync(dto.Id);
+                if (caffeInfo == null)
+                {
+                    return new ResponseDto<object> { Success = false, Data = null, Message = "Kafe bilgisi bulunamadı", ErrorCode = ErrorCodes.NotFound };
+                }
+                var result = _mapper.Map(dto, caffeInfo);
+                await _caffeInfoRepository.UpdateAsync(result);
+                return new ResponseDto<object> { Success = true, Data = null, Message = "Kafe bilgisi başarıyla güncellendi" };
+
+            }
+            catch (Exception ex)
+            {
+                return new ResponseDto<object> { Success = false, Data = null, Message = "Bir Hata Oluştu", ErrorCode = ErrorCodes.Exception };
+            }
         }
     }
 }
---
+++ Core/CaffeAPI.Aplication/Validators/CaffeInfo/UpdateCaffeInfoValidator.cs
@@ -0,0 +1,37 @@
+﻿using CaffeAPI.Aplication.Dtos.CaffeInfoDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaffeAPI.Aplication.Validators.CaffeInfo
+{
+    public class UpdateCaffeInfoValidator : AbstractValidator<UpdateCaffeInfoDto>
+    {
+        public UpdateCaffeInfoValidator()
+        {
+            RuleFor(x => x.Name)
+             .NotEmpty()
+              .WithMessage("Laffe adi boş olamaz")
+              .MaximumLength(100)
+              .WithMessage("Kafe adı en fazla 100 karakter olmalıdır");
+            RuleFor(x => x.Address)
+                .NotEmpty()
+                .WithMessage("Kafe adresi boş olamaz")
+                .MaximumLength(500)
+                .WithMessage("Kafe adresi en fazla 500 karakter olmalıdır");
+            RuleFor(x => x.PhoneNumber)
+                .NotEmpty()
+                .WithMessage("Kafe telefon numarası boş olamaz")
+                .Matches(@"^\+?[0-9]{10,15}$")
+                .WithMessage("Kafe telefon numarası geçerli bir formatta olmalıdır (örn: +1234567890)");
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Kafe e-posta adresi boş olamaz")
+                .EmailAddress()
+                .WithMessage("Kafe e-posta adresi geçerli bir formatta olmalıdır");
+        }
+    }
+}
